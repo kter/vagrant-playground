bcc3ba184a3b0f3732db9275809e86a03108136c
kter <me@kter.jp>
2014-05-18 17:04:39 +0900

    rpmforgeリポジトリを追加

--- modules/repository/manifests/init.pp
+++ modules/repository/manifests/init.pp
@@ -1,6 +1,10 @@
 class repository {
-  exec { 'install additional repos':
+  exec { 'install remi repository':
     unless => '/bin/ls -l /etc/yum.repos.d/ | /bin/grep "remi.repo"',
     command => '/bin/rpm -Uvh http://remi.kazukioishi.net/enterprise/remi-release-6.rpm --force',
+  }
+  exec { 'install rpmforge repository':
+    unless => '/bin/ls -l /etc/yum.repos.d/ | /bin/grep "rpmforge.repo"',
+    command => '/bin/rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm --force',
   }
 }
