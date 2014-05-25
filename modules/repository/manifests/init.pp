904c4afe6ed290ac13c7dd52adbc2dcee4c1d17b
kter <me@kter.jp>
2014-05-25 19:54:56 +0900

    add repel repository(needed by remi repository)

--- modules/repository/manifests/init.pp
+++ modules/repository/manifests/init.pp
@@ -7,4 +7,8 @@ class repository {
     unless => '/bin/ls -l /etc/yum.repos.d/ | /bin/grep "rpmforge.repo"',
     command => '/bin/rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm --force',
   }
+  exec { 'install epel repository':
+    unless => '/bin/ls -l /etc/yum.repos.d/ | /bin/grep "remi.repo"',
+    command   => '/bin/rpm -Uvh http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm --force',
+  }
 }
